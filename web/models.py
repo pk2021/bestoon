64f926c59f4243a701b1ffe3c25afb0e1342f77b
pk2021 <pouya_persia021@yahoo.com>
2020-02-17 03:44:37 +0330

    Income and Extense and some beautification

--- web/models.py
+++ web/models.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    text = models.CharField(blank=True, max_length=100)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return '{}-{}'.format(self.date, self.amount)
+
+class Income(models.Model):
+    text = models.CharField(blank=True, max_length=100)
+    date = models.DateTimeField(blank=True)
+    amount = models.BigIntegerField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return '{}-{}'.format(self.amount, self.text)
